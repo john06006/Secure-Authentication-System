4c98350f2a6841fec7e2603bbcc48e33ebc63b96
john06006 <john63142006@gmail.com>
2025-07-11 20:00:16 +0530

    Add files via upload

---
+++ routes/auth_routes.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from models.user import User
+from models.database import db
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'])
+    new_user = User(username=data['username'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(message='User registered successfully'), 201
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if user and check_password_hash(user.password, data['password']):
+        access_token = create_access_token(identity=user.username)
+        return jsonify(access_token=access_token), 200
+    return jsonify(message='Invalid credentials'), 401
+
+@auth_bp.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
